54d5b982e14bca890126cdb221ff560221d4f4a1
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-03-12 13:39:03 -0700

    test input fields and failed validation

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test('Renders Form Fields and can input the textfields', () => {
+    render(<ContactForm/>);
+})
